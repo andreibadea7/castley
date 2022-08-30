bb7a1ab9801ae0f017471fa5bce3f0da6a8e7648
Edward Treece <edtreece@gmail.com>
2022-08-30 10:24:25 +0100

    Castle controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "castles#index"
+  root to: "castles#home_page"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # resources :castles, only: [:index]
+  resources :castles, only: [:index, :new, :create, :show]
   # Defines the root path route ("/")
 end
