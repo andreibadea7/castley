292cfd18723069a62726fea9a72bc07af311d5d0
Andrei Badea <andreinbadea7@gmail.com>
2022-08-31 15:36:58 +0100

    index page changes

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -6,13 +6,13 @@ class CastlesController < ApplicationController
   def index
     @castles = Castle.all
     # The `geocoded` scope filters only flats with coordinates
-    # @markers = @castles.geocoded.map do |castle|
-    #   {
-    #     lat: castle.latitude,
-    #     lng: castle.longitude,
-    #     info_window: render_to_string(partial: "info_window", locals: { castle: castle })
-    #   }
-    # end
+    @markers = @castles.geocoded.map do |castle|
+      {
+        lat: castle.latitude,
+        lng: castle.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { castle: castle })
+      }
+    end
   end
 
   def show
