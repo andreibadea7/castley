b930d84bded369316ddb3043c9f6ab3536fe7988
Edward Treece <edtreece@gmail.com>
2022-09-01 10:55:06 +0100

    Changes to user reservation show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_08_31_101110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_101110) do
     t.string "image_url"
     t.float "latitude"
     t.float "longitude"
-
     t.index ["host_id"], name: "index_castles_on_host_id"
   end
 
