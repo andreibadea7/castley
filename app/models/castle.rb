0bf372dbcc25023467ad0e4e4189b5696528bf59
Edward Treece <edtreece@gmail.com>
2022-09-01 14:50:22 +0100

    Reviews showing on castle page

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,6 +1,7 @@
 class Castle < ApplicationRecord
   belongs_to :host, class_name: "User"
   has_many :reservations
+  has_many :reviews, through: :reservations
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_one_attached :photo
