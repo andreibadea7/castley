5aa262de40c531a212659985d8f97da7b3ac42b1
Andrei Badea <andreinbadea7@gmail.com>
2022-09-01 17:04:07 +0100

    complete-seed-file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
-require "open-uri"
 require "json"
+require "open-uri"
 require "nokogiri"
+require "Faker"
 
-url = "https://www.veranda.com/travel/g30083514/beautiful-castles-in-the-world/"
+url = 'https://www.veranda.com/travel/g30083514/beautiful-castles-in-the-world/'
 user_serialized = URI.open(url).read
 # user = JSON.parse(user_serialized)
 html_doc = Nokogiri::HTML(user_serialized)
 
-html_doc.search(".listicle-slide").each_with_index do |element, index|
+html_doc.search(".listicle-slide").each do |element|
   castle_name = element.search(".listicle-slide-hed-text").text.strip.split(" in")[0]
   castle_address = element.search(".listicle-slide-hed-text").text.split(" in")[1].strip
   image_wrapper = element.search(".slide-image-wrap source")
@@ -24,4 +25,26 @@ html_doc.search(".listicle-slide").each_with_index do |element, index|
                 has_swimming_pool: [true, false].sample,
                 has_horses: [true, false].sample,
                 capacity: rand(5..25) * 10)
+end
+
+10.times do
+  check_in = Date.new(rand(1977..2021), rand(1..12), rand(1..28))
+  check_out = check_in + rand(1..100)
+  Reservation.create(
+    check_in: check_in,
+    check_out: check_out,
+    total_price: rand(5000..50000) * (check_out - check_in),
+    guest_id: User.all.sample.id,
+    castle_id: Castle.all.sample.id
+  )
+end
+
+reservations_without_ids = Reservation.all.ids - Review.all.pluck(:reservation_id)
+
+reservations_without_ids.count.times do
+  Review.create(
+    reservation_id: reservations_without_ids.sample,
+    rating: rand(1..5),
+    comment: Faker::Quote.jack_handey
+  )
 end
