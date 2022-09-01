df53c692704543f4adce43cf2442b86fbe89e817
Andrei Badea <andreinbadea7@gmail.com>
2022-09-01 15:33:32 +0100

    index-map-modifications

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -20,10 +20,10 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window).style
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // Add this
+        .setPopup(popup)
         .addTo(this.map)
     });
   }
@@ -31,6 +31,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3000 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
